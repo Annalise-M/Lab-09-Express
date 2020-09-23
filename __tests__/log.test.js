e32d0649c9de6583a7fd27159c6339df2a932b18
Annalise Murphy <nali247@gmail.com>
2020-09-22 20:35:54 -0700

    gets log by id via GET passing

--- __tests__/log.test.js
+++ __tests__/log.test.js
@@ -6,18 +6,16 @@ const Recipe = require('../lib/models/recipe');
 const Log = require('../lib/models/Log');
 
 describe('log-lab routes', () => {
-  beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
-  });
-
-  it('creates a log via POST', async() => {
-    
-    const recipes = await Promise.all([
+  beforeEach(async () => {
+    await pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+    await Promise.all([
       { name: 'cookies', directions: [] },
       { name: 'cake', directions: [] },
       { name: 'pie', directions: [] }
     ].map(recipe => Recipe.insert(recipe)));
+  });
 
+  it('creates a log via POST', async() => {
     return request(app)
       .post('/api/v1/logs')
       .send(
@@ -38,7 +36,37 @@ describe('log-lab routes', () => {
           notes: 'here are some notey note notes notesssssss',
           rating: 4
         });
-      });
+      })
   });
+
+  it('gets log by id via GET', async() => {
+    const log = await Log.insert({
+      id: expect.any(String),
+      recipe_id: '2',
+      date_of_event: '2020-09-22',
+      notes: 'here are some notey note notes notesssssss',
+      rating: 4
+    });
   
+    const response = await request(app)
+      .get(`/api/v1/logs/${log.id}`);
+
+    expect(response.body).toEqual(log);
+  });
+
+  // it('gets all logs via GET', async() => {
+  //   const logs = await Promise.all([
+  //     { name: 'cookies', directions: [] },
+  //     { name: 'cake', directions: [] },
+  //     { name: 'pie', directions: [] }
+  //   ].map(recipe => Recipe.insert(recipe)));
+
+  //   return request(app)
+  //     .get('/api/v1/logs')
+  //     .then(res => {
+  //       logs.forEach(recipe => {
+  //         expect(res.body).toContainEqual(log);
+  //       });
+  //     });
+  // });
 });
